6675909032f0aa738b29276d45b6413399f75384
sw478 <sw478@users.noreply.github.com>
2022-04-26 11:53:42 +0000

    deploy: 5d4223394d3a3f257caac8933ca573c7729a0455

--- hrowCover.h
+++
@@ -1,8 +0,0 @@
-#ifndef HROWCOVER_H
-#define HROWCOVER_H
-#include "struct.h"
-
-void coverRowHeaders(Dance *d);
-void uncoverRowHeaders(Dance *d);
-
-#endif
--- initDance.h
+++
@@ -1,7 +0,0 @@
-#ifndef INIT_DANCE_H
-#define INIT_DANCE_H
-#include "struct.h"
-
-int initDance(Dance *d);
-
-#endif
